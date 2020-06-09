0b6a33de02d158c06ed2d7dd1c73ee4b0e3cf45b
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-06-09 13:02:39 -0300

    teste na page feed

--- labeddit/src/components/FeedPage.js
+++ labeddit/src/components/FeedPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Axios from 'axios'
 
 const Container = styled.div`
     height: 200px;
@@ -11,10 +12,18 @@ const Container = styled.div`
 `
 
 function FeedPage() {
+    const verLista = () => {
+        Axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labEddit/posts', 
+        {headers:{'auth': localStorage.getItem('token')}})
+        .then(Response => {console.log('Get Post:', Response.data)})
+    }
+
     const history = useHistory()
 
     useEffect(() => {
+        
         localStorage.getItem('token') === null && history.push('/')
+        verLista ()
     })
 
     const goToPost = () => {
@@ -37,6 +46,7 @@ function FeedPage() {
             <button onClick={post}>POSTAR</button>
             <button onClick={goToPost}>POST</button>
             <button onClick={goToLogin}>SAIR</button>
+            
         </Container>
     );
 }
