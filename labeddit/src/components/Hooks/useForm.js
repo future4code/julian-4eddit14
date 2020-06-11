c1d413088734fe6e5af273b8913e71810d238022
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-06-10 22:58:01 -0300

    alteração no UseForm e no Login

--- labeddit/src/components/Hooks/useForm.js
+++ labeddit/src/components/Hooks/useForm.js
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 
-export const useForm = initialValues => {
-    const [form, setForm] = useState(initialValues)
-
-    const onChange = (name, value) => {
-        const newForm = { ...form, [name]: value }
-        setForm(newForm)
+export const useForm = (initialState) => {
+    const [form, setForm] = useState(initialState)
+  
+    const onChange = (event) => {
+      const newValue = event.target.value
+      const fieldName = event.target.name
+  
+      setForm({...form, [fieldName]: newValue})
     }
 
     const resetValues = () => {
-        setForm(initialValues)
+        setForm(initialState)
     }
 
     return { form, onChange, resetValues }
