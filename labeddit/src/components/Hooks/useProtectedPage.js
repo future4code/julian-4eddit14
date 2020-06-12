227c195d5683af28c2df25985812b3f509081131
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-06-12 14:57:14 -0300

    Hook useProtectedPage funcionando em feePage e PostPage

--- labeddit/src/components/Hooks/useProtectedPage.js
+++ labeddit/src/components/Hooks/useProtectedPage.js
@@ -9,7 +9,7 @@ export const useProtectedPage = () => {
         const token = window.localStorage.getItem('token')
 
         if(!token){
-            history.push('/login')
+            history.push('/')
         }
-    })
+    }, [])
 }
